9a202a8d69696ffae35cd72b50bac9bf809ab58a
Patopop007 <plankenau@gmail.com>
2012-06-21 08:46:54 -0500

    Added fakeData for testing purposes, as well as load function

--- application/models/log.php
+++ application/models/log.php
@@ -9,13 +9,27 @@
 class Log extends CI_Model{
     var $id = 0;
     var $owner = "";
-    var $entries = "";
+    var $entries = array();
+    var $entriestext = "";
 
     function Log(){
         parent::__construct();
     }
     function load($id){
-        
+        if ($id==0) 
+            $this->fakeData();
+    }
+    function fakeData(){
+        $this->entries = array(
+            array(
+                'date' => "07/07/2012",
+                'title' => "Wore Shoes"
+            ),
+            array(
+                'date' => "07/08/2012",
+                'title' => "Wore bigger shoes"
+            )
+        );
     }
 }
 ?>
