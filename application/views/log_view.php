8ec7bccf1fdd8528a071a0c9c8fa2a3233fbbed0
Pato Lankenau <plankenau@gmail.com>
2012-07-31 12:41:53 -0500

    Created log page with styled table

--- application/views/log_view.php
+++ application/views/log_view.php
@@ -2,9 +2,10 @@
 /*
  * ##############CHANGELOG###########################################################################
  * May 18, 2012 - Created page
+ * July 28 - Create table based on log data, each entry is its own form
+ * July 29 - Created on big form for all entires, count of entires, as well as 5 empty entries at the bottom, added tablecloth
  *
  * ###################################################################################################
- * +
  */
 
 ?>
@@ -12,6 +13,17 @@
 <head>
     <title>IB CAS Organizer</title>
     <link rel="stylesheet" href="/css/style.php" type="text/css"/>
+    <link href="../tablecloth/tablecloth.css" rel="stylesheet" type="text/css" media="screen" />
+    <script type="text/javascript" src="../tablecloth/tablecloth.js"></script>
+    <script type="text/javascript">
+    window.onload = function(){
+        new JsDatePick({
+                useMode:2,
+                target:"date1",
+                dateFormat:"%Y-%m-%d",
+                limitToToday:true
+        });
+    </script>
 </head>
 <body>
 <center>
@@ -22,10 +34,36 @@
         </div>
         <?php $this->load->view("menu"); ?>
         <div class="container">
-            <h3>Events</h3>
-            <?php foreach ($entries as $entry):?>
-            <p><?=$entry['title']?></p>
+            <h3>Entries</h3>
+            <form action="/home/update_log" method="POST">
+            <table cellspacing="0" cellpadding="0">
+                <tr>
+                    <th>Activity</th>
+                    <th>Date</th>
+                    <th>Time</th>
+                    <th>Hours</th>
+                </tr>
+            <?php $count = 0; foreach ($entries as $entry): $count+=1; ?>
+                <tr>
+                    <td><input type="text" name="title<?=$count?>" value="<?=$entry['title']?>" size="40"></td>
+                    <td><input type="text" name="date<?=$count?>" value="<?=$entry['date']?>"></td>
+                    <td><input type="text" name="time<?=$count?>" value="<?=$entry['time']?>"></td>
+                    <td><input type="text" name="hours<?=$count?>" value="<?=$entry['hours']?>" size="10"></td>
+                </tr>
             <?php endforeach; ?>
+            <?php for($i=0;$i<5;$i++): $count+=1;?>
+                <tr>
+                    <td><input type="text" name="title<?=$count?>" value="" size="40"></td>
+                    <td><input type="text" name="date<?=$count?>" value=""></td>
+                    <td><input type="text" name="time<?=$count?>" value=""></td>
+                    <td><input type="text" name="hours<?=$count?>" value="" size="10"></td>
+                </tr>
+            <?php endfor; ?>
+            </table>
+            <input type="hidden" name="id" value="<?=$id?>">
+            <input type="hidden" name="count" value="<?=$count?>">
+            <input type="submit" class="btn1" value="Save Changes">
+            </form>
         </div>
         <?php $this->load->view("footer"); ?>
 
