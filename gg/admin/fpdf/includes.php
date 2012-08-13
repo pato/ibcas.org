925c3efbc108cb726b28eae6d8cab23812fdc34d
Pato Lankenau <plankenau@gmail.com>
2012-08-13 14:01:12 -0500

    Removed GammaGames sandbox
    Not necessary anymore, not part of this project

--- gg/admin/fpdf/includes.php
+++
@@ -1,5 +0,0 @@
-<?php
-//require('fpdf/fpdf.php');
-//require('fpdf/html2pdf.php');
-require('fpdf/html_table.php');
-?>
--- gg/admin/security.php
+++
@@ -1,9 +0,0 @@
-<?php
-session_start();
-
-if(isset($_SESSION['alive']) && $_SESSION['alive']==true){
-	//logged in
-}else{
-	header( 'Location: ./login.php' );
-}
-?>
