1aaec9312a0b4a65fb2995008364daaba619e193
Pato Lankenau <plankenau@gmail.com>
2012-08-18 00:00:21 -0500

    Custom 404 error

---
+++ application/views/404.php
@@ -0,0 +1,39 @@
+<?php
+/*
+ * ##############CHANGELOG###########################################################################
+ * Aug 17, 2012 - Created page
+ *
+ * ###################################################################################################
+ * +
+ */
+
+?>
+<html>
+<head>
+    <title>IB CAS Organizer</title>
+    <link rel="stylesheet" href="/css/style.php" type="text/css"/>
+</head>
+<body>
+<center>
+<div class="pageContent">
+    <div id="main">
+	<div class="container">
+            <h1>404 - Did you divide by zero?</h1>
+        </div>
+        <div class="container">
+            <br>
+            <br>
+            <br>
+            <h3>This page no longer exists...</h3>
+            <h3>This page never existed...</h3>
+            <h3>You were never here...</h3>
+            <br>
+            <br>
+            <h1><a href="/anizer">Go Home</a></h1>
+        </div>
+
+    </div>
+</div>
+</center>
+</body>
+</html>
